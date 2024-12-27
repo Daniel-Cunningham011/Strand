6a0f0aa8e3a60b52143863d618131ad21127c534
Daniel-Cunningham011 <danrcunn011@gmail.com>
2024-12-27 15:40:16 -0600

    Working on Rope Data Structure still.

--- strand_ropes/lib.rs
+++ strand_ropes/lib.rs
@@ -80,6 +80,12 @@ impl TreeComponent for Rope {
 }
 
 impl Rope {
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            size: 0,
+        } 
+    }
     pub fn head(&self) -> Option<Node> {
         if let Some(x) = &self.head {
             return Some(*x.clone())
@@ -87,29 +93,14 @@ impl Rope {
         None
     }
 
-    fn traverse_left(&self, stack: &mut NodeStack, collected_string: &mut String, current_node: Option<Node>) {
-        let mut finished_traverse = false; 
-        let mut head = current_node;
-        while finished_traverse != true {
-            stack.push(head.clone().unwrap());
-            head = match self.left() {
-                Some(node) => Some(node),
-                None => {finished_traverse = true; head}, 
-            };
-        }
-        *collected_string += head.unwrap().value.as_str(); 
-    }
-   
-
-
-    pub fn collect(&self) -> String {
+    pub fn collect(&self) -> Vec<Node> {
         let mut stack = NodeStack::new(); 
         let mut head = match self.head() {
-            None => return String::new(),
+            None => return Vec::new(),
             Some(x) => Some(x)
         };
         
-        let data: String = String::new();
+        let data: Vec<Node> = Vec::new();
         self.traverse_left(&mut stack.clone(), &mut data.clone(), head); 
         while !stack.empty() {
             match stack.pop() {
@@ -127,4 +118,62 @@ impl Rope {
         return data; 
     }
 
+    pub fn rebalance(&mut self) -> Self {
+       Self::new() 
+    }
+
+    pub fn depth(&self) -> usize {
+        if let Some(head_node) = self.head() {
+           let left_depth = match head_node.left() {
+                Some(left_node) => 1 + self.depth_helper(left_node),
+                None => 0
+           };
+           let right_depth = match head_node.right() {
+                Some(right_node) => 1 + self.depth_helper(right_node),
+                None => 0
+           };
+           let maximum = match left_depth > right_depth {
+                true => left_depth,
+                false => right_depth
+           };
+           return maximum;
+        }
+        0
+    }
+
+    fn depth_helper(&self, current_node: Node) -> usize {
+        let left_depth = match current_node.left() {
+            Some(left_node) => 1 + self.depth_helper(left_node),
+            None => 0
+        };
+        let right_depth = match current_node.right() {
+            Some(right_node) => 1 + self.depth_helper(right_node),
+            None => 0
+        };
+        let maximum = match left_depth > right_depth {
+            true => left_depth,
+            false => right_depth
+        };
+        return maximum;
+    }
+
+    // 2^n = height =>  log_2(height) = n
+
+    fn isBalanced(&self) -> bool {
+        let depth = self.depth();
+        true     
+    }
+
+    fn traverse_left(&self, stack: &mut NodeStack, data: &mut Vec<Node>, current_node: Option<Node>) {
+        let mut finished_traverse = false; 
+        let mut head = current_node;
+        while finished_traverse != true {
+            stack.push(head.clone().unwrap());
+            head = match self.left() {
+                Some(node) => Some(node),
+                None => {finished_traverse = true; head}, 
+            };
+        }
+        data.push(head.unwrap()); 
+    }
 }
